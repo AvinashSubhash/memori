478b4e45b02beb73559d4abcfb988632fff7f4d5
AvinashSubhash <avinashsubhash19@gmail.com>
2026-07-20 23:08:34 +0530

    remove comment

--- database.go
+++ database.go
@@ -30,7 +30,6 @@ func ConnectDatabase() {
 	fmt.Println(" Connecting ", dsn)
 
 	//
-	//dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	//
 
